d6bf16350bfa0ced3daf7575079999e0c223c74c
AQUIFERAQ <99050856+AQUIFERAQ@users.noreply.github.com>
2023-05-01 18:20:45 +0300

    fullstack

---
+++ script .js/script.js
@@ -0,0 +1,12 @@
+/*
+
+const btn = document.querySelector('.btn');
+const navLink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => {
+    navLink.classList.toggle('hide');
+});
+})*/
+
+const btn = document.querySelector('.btn' ).innerHTML='change';
+
